b96225940dbc8413f94469cdcb22540039fd38ce
Melih Cüneyter <melihcuneyter@hotmail.com>
2023-01-01 19:14:45 +0300

    Initial Project.

---
+++ week4hw2/week4hw2/BaseVC.swift
@@ -0,0 +1,16 @@
+//
+//  BaseVC.swift
+//  week4hw2
+//
+//  Created by Melih Cüneyter on 1.01.2023.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
